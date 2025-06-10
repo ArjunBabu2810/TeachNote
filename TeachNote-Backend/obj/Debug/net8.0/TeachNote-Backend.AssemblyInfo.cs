bafcf006399971a4d76048f4d978efa4c893fdf4
ArjunBabu2810 <arjunvellaikudam@gmail.com>
2025-06-10 21:39:12 +0530

    updated appsettings json file

--- TeachNote-Backend/obj/Debug/net8.0/TeachNote-Backend.AssemblyInfo.cs
+++ TeachNote-Backend/obj/Debug/net8.0/TeachNote-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb7e24c7792bd37175276f6b41e3eeb237b4365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+242f24b746265b3156546608184540a89313f812")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
