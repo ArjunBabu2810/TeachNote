f7ae29a76e30471500375e9c137424a26bb6548d
ArjunBabu2810 <arjunvellaikudam@gmail.com>
2025-06-10 13:03:21 +0530

    example2

--- TeachNote-Backend/obj/Debug/net8.0/TeachNote-Backend.AssemblyInfo.cs
+++ TeachNote-Backend/obj/Debug/net8.0/TeachNote-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bddf4769c10845f58e6d140d0f38edaf3d6c30e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c66cec1cf62c4f14f9b91c39eb7901105a2a85d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
