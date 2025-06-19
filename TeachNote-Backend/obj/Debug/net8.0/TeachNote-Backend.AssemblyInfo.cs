83ead55c61eca290dff43aa6f8aa6792e587c950
ArjunBabu2810 <arjunvellaikudam@gmail.com>
2025-06-19 15:28:49 +0530

    none

--- TeachNote-Backend/obj/Debug/net8.0/TeachNote-Backend.AssemblyInfo.cs
+++ TeachNote-Backend/obj/Debug/net8.0/TeachNote-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30f996b00668dd223f896763961635a3a8d6a50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e53ec7bfeeab60b2b79f606a684f53d6b82e3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeachNote-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
